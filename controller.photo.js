7a503d99a493e22843106e8bc187c9e81b2b8eb1
James Harris <computersherpa@gmail.com>
2017-01-20 12:03:48 -0600

    Show images and info

--- controller.photo.js
+++ controller.photo.js
@@ -3,6 +3,13 @@ widgets.controller("PhotoCtrl",
   ['$scope',
     function($scope){
       $scope.rawFeed = instagramResponse;
+      $scope.images = $scope.rawFeed.data;
+      $scope.showImage = function showImage(imageData) {
+        imageData.visible = true;
+      };
+      $scope.hideImage = function hideImage(imageData) {
+        imageData.visible = false;
+      }
     }
   ]
 )
